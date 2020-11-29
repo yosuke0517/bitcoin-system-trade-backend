2c60762d2c25ccf0dee11bc73ec2a08616052ec5
takeuchiyosuke <x.clover.x007@gmail.com>
2020-11-29 20:19:15 +0900

    feat: オープン条件変更

--- application/controllers/bitflyerController.go
+++ application/controllers/bitflyerController.go
@@ -8,8 +8,10 @@ import (
 	"time"
 )
 
+var tradeTicker bitflyer.Ticker
+
 func StreamIngestionData() {
-	ai := NewAI(os.Getenv("PRODUCT_CODE"), config.Config.Durations["1m"], config.Config.DataLimit, config.Config.UsePercent, config.Config.StopLimitPercent, config.Config.BackTest)
+	// ai := NewAI(os.Getenv("PRODUCT_CODE"), config.Config.Durations["1m"], config.Config.DataLimit, config.Config.UsePercent, config.Config.StopLimitPercent, config.Config.BackTest)
 
 	var tickerChannl = make(chan bitflyer.Ticker)
 	bitflyerClient := bitflyer.New(os.Getenv("API_KEY"), os.Getenv("API_SECRET"))
@@ -18,12 +20,25 @@ func StreamIngestionData() {
 		for {
 			for ticker := range tickerChannl {
 				for _, duration := range config.Config.Durations {
+					tradeTicker = ticker
 					isCreated := service.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
-					if isCreated == true && time.Now().Second()%10 == 0 {
-						ai.Trade(ticker)
+					//if isCreated == true && duration == config.Config.Durations["30s"] {
+					//	ai.Trade(ticker)
+					//}
+					if isCreated == true {
 					}
 				}
 			}
 		}
 	}()
+}
+
+func Test() {
+	ai := NewAI(os.Getenv("PRODUCT_CODE"), config.Config.Durations["1m"], config.Config.DataLimit, config.Config.UsePercent, config.Config.StopLimitPercent, config.Config.BackTest)
+	for range time.Tick(1 * time.Second) {
+		if time.Now().Second()%10 == 0 {
+			ai.Trade(tradeTicker)
+		}
+	}
+
 }
