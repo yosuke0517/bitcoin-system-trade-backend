1fdd463baa52324e5cdb04d613224846b41413fa
yosuke0517 <x.clover.x007@gmail.com>
2021-01-27 01:48:28 +0900

    feat: クローズ判定精査

--- application/controllers/aiTrade.go
+++ application/controllers/aiTrade.go
@@ -307,7 +307,7 @@ func (ai *AI) Trade(ticker bitflyer.Ticker) {
 		log.Println("isCandleOpportunityをtrueにします")
 	}
 	// 5分00秒じゃ無いときかつ、PositionがあるときはProfitでの決済のみ対応する
-	if time.Now().Minute()%15 != 0 || (time.Now().Minute()%5 == 0 && time.Now().Second() > 5) {
+	if time.Now().Minute()%15 != 0 || (time.Now().Minute()%15 == 0 && time.Now().Second() > 5) {
 		isCandleOpportunity = false
 		log.Println("isCandleOpportunityをfalseにします")
 	}
@@ -589,14 +589,14 @@ func (ai *AI) Trade(ticker bitflyer.Ticker) {
 			}
 		}
 		// クローズ時はbuyPoint, sellPointどちらも1以上でParamsをUpdateしてStopLimitを初期化
-		if sellOpen || buyOpen {
-			// sellOpenのクローズ（buyPointにてクローズする場合は15分単位のみ）
-			//if sellOpen == true && (buyPoint > 0 || price <= profit || price >= stopLimit) {
+		// sellOpenのクローズ（buyPointにてクローズする場合は15分単位のみ）
+		//if sellOpen == true && (buyPoint > 0 || price <= profit || price >= stopLimit) {
+		if sellOpen {
 			log.Printf("クローズsellOpen?:%s\n", strconv.FormatBool(sellOpen))
 			log.Printf("クローズショート？？buyPoint > sellPoint:%s\n", strconv.FormatBool(buyPoint > sellPoint))
 			log.Printf("クローズショート？？price <= profit:%s\n", strconv.FormatBool(price <= profit))
-			log.Printf("クローズショート？？総合判定:%s\n", strconv.FormatBool(sellOpen && (buyPoint > 0 && time.Now().Minute()%15 == 0 && time.Now().Second() < 5) || (price <= profit || price >= stopLimit)))
-			if sellOpen && (buyPoint > 0 && time.Now().Minute()%15 == 0 && time.Now().Second() < 5) || (price <= profit || price >= stopLimit) {
+			log.Printf("クローズショート？？総合判定:%s\n", strconv.FormatBool((buyPoint > 0 && time.Now().Minute()%15 == 0 && time.Now().Second() < 5) || (price <= profit || price >= stopLimit)))
+			if (buyPoint > 0 && time.Now().Minute()%15 == 0 && time.Now().Second() < 5) || (price <= profit || price >= stopLimit) {
 				_, isOrderCompleted, _ := ai.Buy(df.Candles[i], price, bbRate)
 				if !isOrderCompleted {
 					log.Printf("ショート：isOrderCompleted == false")
@@ -619,12 +619,14 @@ func (ai *AI) Trade(ticker bitflyer.Ticker) {
 				stopLimit = 0.0
 				// ai.UpdateOptimizeParams(true)
 			}
-			// buyOpenのクローズ（sellPointにてクローズする場合は15分単位のみ）
+		}
+		// buyOpenのクローズ（sellPointにてクローズする場合は15分単位のみ）
+		if buyOpen {
 			log.Printf("クローズbuyOpen?:%s\n", strconv.FormatBool(buyOpen))
 			log.Printf("クローズロングbuyPoint > sellPoint:%s\n", strconv.FormatBool(buyPoint < sellPoint))
 			log.Printf("クローズロングprice >= profit:%s\n", strconv.FormatBool(price >= profit))
-			log.Printf("クローズロング最終判定:%s\n", strconv.FormatBool(buyOpen && (sellPoint > 0 && time.Now().Minute()%15 == 0 && time.Now().Second() < 5) || (price >= profit || price <= stopLimit)))
-			if buyOpen && (sellPoint > 0 && time.Now().Minute()%15 == 0 && time.Now().Second() < 5) || (price >= profit || price <= stopLimit) {
+			log.Printf("クローズロング最終判定:%s\n", strconv.FormatBool((sellPoint > 0 && time.Now().Minute()%15 == 0 && time.Now().Second() < 5) || (price >= profit || price <= stopLimit)))
+			if (sellPoint > 0 && time.Now().Minute()%15 == 0 && time.Now().Second() < 5) || (price >= profit || price <= stopLimit) {
 				_, isOrderCompleted, _ := ai.Sell(df.Candles[i], price, bbRate)
 				if !isOrderCompleted {
 					log.Printf("ロング：isOrderCompleted == false")
@@ -647,26 +649,26 @@ func (ai *AI) Trade(ticker bitflyer.Ticker) {
 				stopLimit = 0.0
 				// ai.UpdateOptimizeParams(true)
 			}
-			// 1つでも買いのインディケータがあれば買い
-			//if buyPoint > 0 {
-			//	_, isOrderCompleted := ai.Sell(df.Candles[i])
-			//	if !isOrderCompleted {
-			//		continue
-			//	}
-			//	buyOpen = false
-			//	stopLimit = 0.0
-			//	ai.UpdateOptimizeParams(true)
-			//}
-			//if sellPoint > 0 {
-			//	_, isOrderCompleted := ai.Buy(df.Candles[i])
-			//	if !isOrderCompleted {
-			//		continue
-			//	}
-			//	sellOpen = false
-			//	stopLimit = 0.0
-			//	ai.UpdateOptimizeParams(true)
-			//}
 		}
+		// 1つでも買いのインディケータがあれば買い
+		//if buyPoint > 0 {
+		//	_, isOrderCompleted := ai.Sell(df.Candles[i])
+		//	if !isOrderCompleted {
+		//		continue
+		//	}
+		//	buyOpen = false
+		//	stopLimit = 0.0
+		//	ai.UpdateOptimizeParams(true)
+		//}
+		//if sellPoint > 0 {
+		//	_, isOrderCompleted := ai.Buy(df.Candles[i])
+		//	if !isOrderCompleted {
+		//		continue
+		//	}
+		//	sellOpen = false
+		//	stopLimit = 0.0
+		//	ai.UpdateOptimizeParams(true)
+		//}
 	}
 }
 
